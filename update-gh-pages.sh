9803169aebc7ad2cc9474456396ff5e9d5d8a2f2
Yao-Po Wang <blue119@gmail.com>
2014-04-06 17:45:04 +0800

    udate update-gh-pages.sh

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -13,12 +13,12 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     git config --global user.name "Travis"
 
     #using token clone gh-pages branch
-    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/blue119/robot-travis.git  gh-pages > /dev/null
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/blue119/robot-travis.git gh-pages > /dev/null
 
     #go into diractory and copy data we're interested in to that directory
     cd gh-pages
-    cp -f home/travis/build/blue119/robot-travi/RobotDemo/log.html .
-    cp -f home/travis/build/blue119/robot-travi/RobotDemo/report.html .
+    cp -f $HOME/build/blue119/robot-travi/RobotDemo/log.html .
+    cp -f $HOME/build/blue119/robot-travi/RobotDemo/report.html .
 
     #add, commit and push files
     git add -f .

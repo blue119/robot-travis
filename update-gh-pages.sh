3880d9599a575950e55407cc76a0f76ccfda827f
Yao-Po Wang <blue119@gmail.com>
2014-04-06 14:37:22 +0800

    travis can update report to gh-page

---
+++ update-gh-pages.sh
@@ -0,0 +1,28 @@
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    echo -e "Starting to update gh-pages\n"
+
+    #copy data we're interested in to other place
+    # cp -R coverage $HOME/coverage
+
+    #go to home and setup git
+    echo $HOME
+    cd $HOME
+
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis"
+
+    #using token clone gh-pages branch
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/blue119/robot-travis.git  gh-pages > /dev/null
+
+    #go into diractory and copy data we're interested in to that directory
+    cd gh-pages
+    cp -f home/travis/build/blue119/robot-travi/RobotDemo/log.html .
+    cp -f home/travis/build/blue119/robot-travi/RobotDemo/report.html .
+
+    #add, commit and push files
+    git add -f .
+    git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+    git push -fq origin gh-pages > /dev/null
+
+    echo -e "Done magic with coverage\n"
+fi
